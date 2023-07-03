1b6d460ed57a352abe77b3d0ad71452fa9a47b29
INSP1RIT <inspiritstillok@protonmail.com>
2023-07-03 17:24:36 +0300

    post routes

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["Base", "Post", "SQLALCHEMY_DATABASE_URL", "User"]
 from .db import Base, SQLALCHEMY_DATABASE_URL
-from .models import Post
+from microblog.models import Post
 from user.models import User
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import Response
+
+from core.db import session_local
+from microblog.blog import router as blog_router
 
 app = FastAPI()
 
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = session_local()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    return response
+
+app.include_router(blog_router, prefix="/blog")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+
